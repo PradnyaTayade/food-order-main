a00d76f5ad12dc1bf9955d74505143b584f2e850
PradnyaTayade <88725002+PradnyaTayade@users.noreply.github.com>
2022-04-03 00:04:35 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,17 @@
+    <!-- Footer Selection Starts ---> 
+<div class="footer">
+<div class="share">
+
+    <a href="#" class="btn1">FACEBOOK</a>
+    <a href="#" class="btn1">INSTAGRAM</a>
+    <a href="#" class="btn1">TWITTER</a>
+    <a href="#" class="btn1">PINTEREST</a>
+    <a href="#" class="btn1">LINKEDIN</a>
+</div>
+<h1 class="credit"> created by<span> name of creator</span> | all rights reserved!</h1>
+</div>
+</div>
+
+<!-- Footer Selection Ends --->
+</body>
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,28 @@
+<?php 
+      include('../config/constants.php'); 
+      //include('login-check.php');  //1.for login we have to comment this line if we are creating database go to login.php
+
+ ?>
+
+<html>
+<head>
+<title>Food Order Website - Home Page</title>
+
+<link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+<!--Menu Section Starts -->
+<div class="menu text-centre"> 
+<div class="wrapper">
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="manage-admin.php">Admin</a></li>
+<li><a href="manage-category.php">Category</a></li>
+<li><a href="manage-food.php">Food</a></li>
+<li><a href="user-info.php">Users</a></li>
+<li><a href="manage-order.php">Order</a></li>
+<li><a href="logout.php">Logout</a></li>
+</ul>
+</div>
+</div>
