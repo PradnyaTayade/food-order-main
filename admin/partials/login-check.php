7cb1945137597bd184211bc2bd0925e60e81318e
PradnyaTayade <88725002+PradnyaTayade@users.noreply.github.com>
2022-04-17 12:44:27 +0530

    Update login-check.php

--- admin/partials/login-check.php
+++ admin/partials/login-check.php
@@ -1,11 +1,11 @@
 <?php
 //<!--> comment this all 
-   /* if(!isset($_SESSION['user']))
+   if(!isset($_SESSION['user']))
      {
           
      $_SESSION['no-login']="<div class='error' text='center'>Please login to access Admin Panel.</div>";
 
      header('location:'.SITEURL.'admin/login.php');
-     }*/
+     }
 
 ?>
